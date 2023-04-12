9802b97abc967f6074d2b5dd814f84f15eca8088
Ruby Simpleman <rubysimpleman@gmail.com>
2023-04-11 18:56:05 -0700

    2023-04-11_18-56-05_smallChanges

--- js/index.js
+++ js/index.js
@@ -1,21 +1,3 @@
-const touchZoneFirst = document.querySelector('.touchZoneFirst__css-class');
-touchZoneFirst.addEventListener('touchstart', touchSTARTfunctionLushchenko)
-touchZoneFirst.addEventListener('touchend', touchENDfunctionLushchenko)
-touchZoneFirst.addEventListener('touchmove', touchMOVEfunctionLushchenko)
-
-function touchSTARTfunctionLushchenko(event) {
-  console.log(event)
-  console.log('touch')
-  document.querySelector('.textOutputZoneFirst__css-class p').textContent += event.touches.length
-}
-function touchENDfunctionLushchenko(event) {
-  document.querySelector('.textOutputZoneFirst__css-class p').textContent += ` end `
-}
-function touchMOVEfunctionLushchenko(event) {
-  document.querySelector('.textOutputZoneFirst__css-class p').textContent += ` M `
-}
-
-
 const container = document.getElementById('container')
 const SQUARES = 500
 const extraSQUARES = 4
@@ -42,86 +24,85 @@ for (let i=0; i < SQUARES; i++) {
   })
 }
 
-        // for (let i=0; i < SQUARES; i++) {
-          // const square = document.createElement('div')
-          // square.classList.add('square')
-  
-  // square.addEventListener('mouseover', () => {
-  //   setColor(square)
-  //   removeColor(square)
-  // })
-  // square.addEventListener('mouseout', () => {
-  //   setColor(square)
-  //   removeColor(square)
-  // })
-  // square.addEventListener('mousedown', () => {
-  //   setColor(square)
-  //   removeColor(square)
-  // })
-  // square.addEventListener('mousemove', () => {
-  //   setColor(square)
-  //   removeColor(square)
-  // })
-  // square.addEventListener('mouseup', () => {
-  //   setColor(square)
-  //   removeColor(square)
-  // })
-  // square.addEventListener('click', () => {
-  //   setColor(square)
-  //   removeColor(square)
-  // })
+for (let i=0; i < SQUARES; i++) {
+  const square = document.createElement('div')
+  square.classList.add('square')
 
-          // square.addEventListener('touchstart', () => {
-            // setColor(square)
-          // })
-  // square.addEventListener('touchmove', (event) => {
-  //   event.currentTarget.setColor(square)
-  // })
-  // square.addEventListener('touchend', (event) => {
-  //   event.currentTarget.removeColor(square)
-  // })
+  square.addEventListener('mouseover', () => {
+    setColor(square)
+    removeColor(square)
+  })
+  square.addEventListener('mouseout', () => {
+    setColor(square)
+    removeColor(square)
+  })
+  square.addEventListener('mousedown', () => {
+    setColor(square)
+    removeColor(square)
+  })
+  square.addEventListener('mousemove', () => {
+    setColor(square)
+    removeColor(square)
+  })
+  square.addEventListener('mouseup', () => {
+    setColor(square)
+    removeColor(square)
+  })
+  square.addEventListener('click', () => {
+    setColor(square)
+    removeColor(square)
+  })
 
-  // for (let index=0; index < extraSQUARES; index++) {
-  //   const extraSquare = document.createElement('div')
-  //   extraSquare.classList.add('extraSquare')
-  //   extraSquare.addEventListener('mouseover', () => {
-  //     setColor(extraSquare)
-  //   })
-  //   extraSquare.addEventListener('mouseout', () => {
-  //     removeColor(extraSquare)
-  //   })
-  //   square.appendChild(extraSquare)
-  // }
-          // container.appendChild(square)
-        // }
+  square.addEventListener('touchstart', () => {
+    setColor(square)
+  })
+  square.addEventListener('touchmove', (event) => {
+    event.currentTarget.setColor(square)
+  })
+  square.addEventListener('touchend', (event) => {
+    event.currentTarget.removeColor(square)
+  })
 
-function setColor(event) {
-  // event.target.innerHTML = 0
-  // container.innerHTML = 0
-  console.log("touch")
-  console.log(event)
-  console.log(event.target)
-  console.log(event.currentTarget)
-  // const color = getRandomColor()
-  // el.style.background = color
-  // el.style.border = color
+  for (let index=0; index < extraSQUARES; index++) {
+    const extraSquare = document.createElement('div')
+    extraSquare.classList.add('extraSquare')
+    extraSquare.addEventListener('mouseover', () => {
+      setColor(extraSquare)
+    })
+    extraSquare.addEventListener('mouseout', () => {
+      removeColor(extraSquare)
+    })
+    square.appendChild(extraSquare)
+  }
+  container.appendChild(square)
 }
 
-// function setColor(el) {
-  // const color = getRandomColor()
-  // el.style.background = color
-  // el.style.border = color
-// }
+function setColor(el) {
+  const color = getRandomColor()
+  el.style.background = color
+  el.style.border = color
+}
 
-// function removeColor(el) {
-//   el.style.background = 'transparent'
-// }
+function removeColor(el) {
+  el.style.background = 'transparent'
+}
 
-// function getRandomColor() {
-//   return colors[Math.floor(Math.random() * colors.length - 7)]
-// }
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length - 7)]
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  // event.target.innerHTML = 0
+  // container.innerHTML = 0
+  // console.log("touch")
+  // console.log(event)
+  // console.log(event.target)
+  // console.log(event.currentTarget)
+
+
+
+
 
 // function responsEvent(event) {
   // pShort.innerText = event.type
@@ -143,3 +124,23 @@ function setColor(event) {
 // document.addEventListener('touchstart', responsEvent, false)
 // document.addEventListener('touchmove', responsEvent, false)
 // document.addEventListener('touchend', responsEvent, false)
+
+
+
+// const touchZoneFirst = document.querySelector('.touchZoneFirst__css-class');
+// touchZoneFirst.addEventListener('touchstart', touchSTARTfunctionLushchenko)
+// touchZoneFirst.addEventListener('touchend', touchENDfunctionLushchenko)
+// touchZoneFirst.addEventListener('touchmove', touchMOVEfunctionLushchenko)
+
+// function touchSTARTfunctionLushchenko(event) {
+//   console.log(event)
+//   console.log('touch')
+//   document.querySelector('.textOutputZoneFirst__css-class p').textContent += event.touches.length
+// }
+// function touchENDfunctionLushchenko(event) {
+//   document.querySelector('.textOutputZoneFirst__css-class p').textContent += ` end `
+// }
+// function touchMOVEfunctionLushchenko(event) {
+//   console.log(event)
+//   document.querySelector('.textOutputZoneFirst__css-class p').textContent += ` M `
+// }
